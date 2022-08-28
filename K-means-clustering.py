56cd7cb462d47f8609cfd743a5cc912e3c2128b9
Emil Mubarqui <emilmubarqui@gmail.com>
2022-08-28 10:35:18 -0500

    añade funcion de distancia

--- K-means-clustering.py
+++ K-means-clustering.py
@@ -1,2 +1,21 @@
 import numpy as np
 
+def distancia(a,b):
+	'''
+	Mide la distancia euclidiana entre dos vectores
+
+	Argumentos:
+		a: primer vector
+		b: segundo vector
+
+	Returns:
+		dist: distancia entre a y b
+	'''
+	dist = 0
+	for i in range(len(a)):
+		dist += (a[i]-b[i])**2
+		print(dist)
+
+	dist = np.sqrt(dist)
+
+	return dist
